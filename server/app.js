6107f0b3574a5f77da6e0313edafb3faa57fd910
coolgenerator <97279861+coolgenerator@users.noreply.github.com>
2024-07-27 20:16:20 -0700

    Add database connection setup

--- server/app.js
+++ server/app.js
@@ -1,13 +1,16 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const mangoose = require('mongoose');
+const dotenv = require("dotenv");
+dotenv.config();
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -26,6 +29,11 @@ app.use('/users', usersRouter);
 app.use(function(req, res, next) {
   next(createError(404));
 });
+
+// Connect to MongoDB
+mangoose.connect(process.env.DATABASE_URL)
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
 
 // error handler
 app.use(function(err, req, res, next) {
